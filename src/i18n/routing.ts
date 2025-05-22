808d584ced7c8bf9e329711371aaab2dfe10395e
puayongxiang <puayongxiang@oddle.me>
2025-05-22 11:26:44 +0800

    fix: fix routing handler by adding a new en locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,11 +2,11 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["sg", "hk", "my", "tw", "au"],
+  locales: ["en", "sg", "hk", "my", "tw", "au"],
 
   // Used when no locale matches
-  defaultLocale: "sg",
+  defaultLocale: "en",
   localeDetection: true,
   //to remove the locale prefix from the url
-  localePrefix: "always",
+  localePrefix: "as-needed",
 });
